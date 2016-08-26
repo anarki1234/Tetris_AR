4b12fa3440851fbc5277b001a4d1344e44b9a3fe
tim-hub <tim_w@outlook.com>
2016-08-26 12:03:46 +0800

    fix the key setter - and create a new ar camera prefab

--- Assets/_Scripts/ARKeySetter.cs
+++ Assets/_Scripts/ARKeySetter.cs
@@ -1,14 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using Vuforia;
+using System.IO;
 public class ARKeySetter : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake(){
 
+		#if UNITY_EDITOR
 
+		Debug.Log("start to read and set key");
+
+		string apiKey = ReadKey();
+		GetComponent<VuforiaBehaviour> ().SetAppLicenseKey (apiKey);
+
+		#endif
+	}
+
+
+	string ReadKey(){
+		string keyLocation="Builds/apiKey.txt";
+
+		if (File.Exists (keyLocation)) {
+
+			Debug.Log ("there is a file");
+
+
+			return File.ReadAllText (keyLocation);
+
+		} else {
+
+
+			Debug.LogError("Apply key on developer.vuforia.com, " +
+				"and creat a apiKey.txt in Builds folder, and paste it to the txt file");
+
+
+		}
+
+		return null;
 
 	}
-	
 
 }
